4645093d6ac6ff16d4045837968f934e9de0681e
W0ll3 <info.bshelton@gmail.com>
2025-11-02 18:35:41 +0100

    #ADD Adjusted WiFiHelper

--- firmware/lib/WiFiHelper/WiFiHelper.cpp
+++ firmware/lib/WiFiHelper/WiFiHelper.cpp
@@ -1,30 +1,37 @@
 #include "WiFiHelper.h"
 #include "MillisTimer.h"
 
-static unsigned long lastCheck = 0;
-MillisTimer loadingMS(200);
-MillisTimer intervallMS(2000);
+MillisTimer loadingMS(500);
+MillisTimer timeoutMS(10000);
+MillisTimer retryMS(5000);
 
 
 void connectWiFi(const char* ssid, const char* password) {
+  
   Serial.print("Verbinde mit WLAN: ");
   Serial.println(ssid);
 
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    if (loadingMS.ready()) {
-    Serial.print(".");
+    if (loadingMS.ready()) Serial.print(".");
+    if (timeoutMS.ready()) {
+      Serial.println("\nKeine Verbindung – fahre trotzdem fort.");
+    break;
     }
   }
 
-  Serial.println("\nWLAN verbunden!");
-  Serial.print("IP-Adresse: ");
-  Serial.println(WiFi.localIP());
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nWLAN verbunden!");
+    Serial.print("IP-Adresse: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("WLAN nicht verbunden. Offline-Modus aktiv.");
+  }
 }
 
 void maintainWiFi(const char* ssid, const char* password) {
-  if (intervallMS.ready()) {
+  if (retryMS.ready()) {
     if (WiFi.status() != WL_CONNECTED) {
       Serial.println("WLAN getrennt – versuche Neuverbindung...");
       WiFi.begin(ssid, password);
